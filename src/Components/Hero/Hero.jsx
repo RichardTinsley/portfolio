0dbe1150897a3588fcfa3a471421e3d29fa910c6
RichardTinsley <49978658+RichardTinsley@users.noreply.github.com>
2024-07-21 13:01:18 +1000

    radial conic  rotate

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,10 +4,12 @@ export default function Hero() {
 
     return (
         <div id="hero">
-            <div id="hero-text">
+            <div id="hero-card">
+                <div id="hero-text">
                     <h1 className="large-text">Software Engineer <br /> and Web Developer </h1>
                     <h1 className="medium-text">With over 10 years experience.</h1>  
                     <button id="contact"> Contact Me </button>      
+                </div>
             </div>
         </div>
     )
